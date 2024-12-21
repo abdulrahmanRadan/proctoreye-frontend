bf4b6a47fc3147c3e2d441cb0e999ce36fbd8829
abdulrahmanRadan <aboda123li123@gmail.com>
2024-12-22 00:17:53 +0300

    update the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "../dist", // تحديد مجلد الإخراج
-  },
   resolve: {
     alias: {
-      "@": "/app", // قم بتحديث المسار إلى المجلد الذي يحتوي الملفات.
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
